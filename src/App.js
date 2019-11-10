de9db958822574248c13b7ef32a26c82b736475a
ephiepark <ephiepark@ephieparks-MacBook-Air.local>
2019-11-10 15:53:13 -0800

    Adding material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const REAL_TIME_FREQUENCY = 440;
+
+let audioContext = new AudioContext();
+
+let myOscillator = audioContext.createOscillator();
+myOscillator.frequency.value = REAL_TIME_FREQUENCY;
+
+
+myOscillator.start();
+// myOscillator.stop(audioContext.currentTime + 100); // Stop after two seconds
+console.log("hi");
+
+window.addEventListener('mousedown', e => {
+  myOscillator.connect(audioContext.destination);
+  console.log("mousedown");
+});
+window.onmouseup = (e) => {
+  myOscillator.disconnect(audioContext.destination);
+  console.log("mouseup");
+};
+
 function App() {
   return (
     <div className="App">

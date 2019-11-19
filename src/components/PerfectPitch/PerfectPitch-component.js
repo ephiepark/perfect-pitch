f27af4cace1596d61e8516e2fea0da6ed4144b07
ephiepark <ephiepark@ephieparks-MacBook-Air.local>
2019-11-18 23:26:51 -0800

    Add PerfectPitch title

--- src/components/PerfectPitch/PerfectPitch-component.js
+++ src/components/PerfectPitch/PerfectPitch-component.js
@@ -1,7 +1,10 @@
 import React, {useEffect} from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import player from '../../tone/tone';
 
 const useStyles = makeStyles(theme => ({
@@ -12,6 +15,14 @@ const useStyles = makeStyles(theme => ({
     display: 'none',
   },
 }));
+
+function PerfectPitchHeader(props) {
+  return (
+    <Typography variant="h1" component="h2">
+      {'Perfect Pitch'}
+    </Typography>
+  );
+}
 
 function NoteOptionButtons(props) {
   const classes = useStyles();
@@ -42,17 +53,27 @@ function PerfectPitch(props) {
     player.play(curNote, 4, 1000);
   }, [curRound]);
   return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <NoteOptionButtons
-        noteOptions={noteOptions}
-        onNoteOptionClick={onNoteOptionClick}
-      />
-    </Grid>
+    <React.Fragment>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <PerfectPitchHeader/>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <NoteOptionButtons
+          noteOptions={noteOptions}
+          onNoteOptionClick={onNoteOptionClick}
+        />
+      </Grid>
+    </React.Fragment>
   );
 }
 

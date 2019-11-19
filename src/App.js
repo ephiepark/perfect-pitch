d1c5ad23f3f72eea9703b32eea1cac45ed3614c2
ephiepark <ephiepark@ephieparks-MacBook-Air.local>
2019-11-18 23:15:08 -0800

    Move init code to index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import player from './tone/tone';
 import PerfectPitch from './components/PerfectPitch';
 import {NOTE} from './constants/constants';
 
 function App() {
-  const [is_init, setInit] = useState(false);
-
-  useEffect(() => {
-    if (!is_init) {
-      player.init();
-      setInit(true);
-    }
-  });
-
-  return is_init ? <PerfectPitch
+  return <PerfectPitch
     curNote={NOTE.A}
     curRound={0}
     noteOptions={[NOTE.A]}
     onNoteOptionClick={(note) => {console.log(note);}}
-  /> : null;
+  />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import player from './tone/tone';
+
+// Init starts
+player.init();
+// Init ends
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
